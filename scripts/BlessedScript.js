c9af33c207d3670b373fe2af83424c09e3e0aa15
danx0r <danbmil99@gmail.com>
2010-10-22 16:12:57 -0700

    object + textures

--- scripts/BlessedScript.js
+++ scripts/BlessedScript.js
@@ -18,7 +18,7 @@ var Example;
         this.createObject("examples/simple_script/TestScript.js", "Example.TestScript", {
             space: args.space,
             visual: {
-                mesh: "../content/bigbox.dae",
+                mesh: document.URL + "bigbox.dae",  // GLGE not happy camper with relative paths
             }
         });
         
