0426ba5273c02db15c5a035a68f1a0ea0f5a99c3
danx0r <danbmil99@gmail.com>
2010-10-22 13:20:46 -0700

    sleek, slick MVP-style camera action FTW

--- scripts/BlessedScript.js
+++ scripts/BlessedScript.js
@@ -16,9 +16,16 @@ var Example;
 
         this.connect(args, null, Kata.bind(this.connected, this));
 
+        this.keyIsDown = {};
+        this.camSpeed = 0;
         this.cameraPointX=0;
         this.cameraPointY=0;
+        this.cameraPointXInit=0;
+        this.cameraPointYInit=0;
         this.cameraPos=[0,0,0];
+        this.cameraPosInit=this.cameraPos.concat();
+        this.cameraOrient=[0,0,0,1];
+        this.cameraOrientInit=this.cameraOrient.concat();
 
         this.mChatBehavior =
             new Kata.Behavior.Chat(
@@ -74,52 +81,144 @@ var Example;
                 rollcos * pitchcos * yawsin - rollsin * pitchsin * yawcos,
                 rollcos * pitchcos * yawcos + rollsin * pitchsin * yawsin];
     };
-    Example.BlessedScript.prototype._handleGUIMessage = function (channel, msg) {
+    Example.BlessedScript.prototype._handleGUIMessage = function(channel, msg){
         if (msg.msg == "mousedown") {
-            console.log("DEBUG mousedown:", msg, msg.event)
-            this.dragStartX = parseInt(msg.event.x)-this.cameraPointX;
-            this.dragStartY = parseInt(msg.event.y)-this.cameraPointY;
+            if (msg.event.which == 0) 
+                this.leftDown = true;
+            if (msg.event.which == 1) 
+                this.middleDown = true;
+            if (msg.event.which == 2) {
+                this.rightDown = true;
+                this.dragStartX = parseInt(msg.event.x) - this.cameraPointX;
+                this.dragStartY = parseInt(msg.event.y) - this.cameraPointY;
+            }
+        }
+        if (msg.msg == "mouseup") {
+            if (msg.event.which == 0) 
+                this.leftDown = false;
+            if (msg.event.which == 1) 
+                this.middleDown = false;
+            if (msg.event.which == 2) 
+                this.rightDown = false;
         }
         if (msg.msg == "mousemove") {
-            this.cameraPointX = parseInt(msg.event.x) - this.dragStartX;
-            this.cameraPointY = parseInt(msg.event.y) - this.dragStartY;
-            var q = this._euler2Quat(this.cameraPointX*-.25, this.cameraPointY*-.25, 0);
-            console.log("DEBUG -- mousemove:", msg.event.x, "|", this.cameraPointX, "|", this.dragStartX, "|", q);
-            this.mPresence.setOrientation(q);
+            /// Firefox 4 bug: ev.which is always 0, so get it from mousedown/mouseup events
+            if (this.rightDown) {
+                this.cameraPointX = parseInt(msg.event.x) - this.dragStartX;
+                this.cameraPointY = parseInt(msg.event.y) - this.dragStartY;
+                var q = this._euler2Quat(this.cameraPointX * -.25, this.cameraPointY * -.25, 0);
+                this.mPresence.setOrientation(q);
+            }
         }
+        if (msg.msg == "keyup") {
+            this.keyIsDown[msg.event.keyCode] = false;
+            this.mPresence.setVelocity([0, 0, 0]);
+        }
+        
         if (msg.msg == "keydown") {
-            var ang = this.cameraPointX*-.25 *  0.0174532925;
-            var x = Math.sin(ang);
-            var y = Math.cos(ang);
-            switch(msg.event.keyCode) {
-                case 65:
-                    this.cameraPos[0]-=y;
-                    this.cameraPos[2]+=x;
-                    this.mPresence.setPosition(this.cameraPos);
+            var camMat = Kata.QuaternionToRotation(this.mPresence.orientation(new Date()));
+            var camSpeed = 30
+            var camXX = camMat[0][0] * camSpeed;
+            var camXY = camMat[0][1] * camSpeed;
+            var camXZ = camMat[0][2] * camSpeed;
+            var camZX = camMat[2][0] * camSpeed;
+            var camZY = camMat[2][1] * camSpeed;
+            var camZZ = camMat[2][2] * camSpeed;
+            this.keyIsDown[msg.event.keyCode] = true;
+            var k = "" + msg.event.keyCode
+            if (msg.event.shiftKey) 
+                k += "S"
+            if (msg.event.ctrlKey) 
+                k += "C"
+            switch (k) {
+                case "65": // A -- left
+                    //this.cameraPos[0]-=y;
+                    //this.cameraPos[2]+=x;
+                    this.mPresence.setVelocity([-camXX, -camXY, -camXZ]);
                     break;
-                case 68:
-                    this.cameraPos[0]+=y;
-                    this.cameraPos[2]-=x;
-                    this.mPresence.setPosition(this.cameraPos);
+                case "68": // D -- right
+                    //this.cameraPos[0]+=y;
+                    //this.cameraPos[2]-=x;
+                    this.mPresence.setVelocity([camXX, camXY, camXZ]);
+                    //this.mPresence.setPosition(this.cameraPos);
                     break;
-                case 87:
-                    this.cameraPos[0]-=x;
-                    this.cameraPos[2]-=y;
-                    this.mPresence.setPosition(this.cameraPos);
+                case "87": // W -- forward
+                case "38": // up arrow
+                    //this.cameraPos[0]-=x;
+                    //this.cameraPos[2]-=y;
+                    this.mPresence.setVelocity([-camZX, -camZY, -camZZ]);
+                    //this.mPresence.setPosition(this.cameraPos);
                     break;
-                case 83:
-                    this.cameraPos[0]+=x;
-                    this.cameraPos[2]+=y;
-                    this.mPresence.setPosition(this.cameraPos);
+                case "83": // S -- reverse
+                case "40": // down arrow
+                    //this.cameraPos[0]+=x;
+                    //this.cameraPos[2]+=y;
+                    this.mPresence.setVelocity([camZX, camZY, camZZ]);
+                    //this.mPresence.setPosition(this.cameraPos);
                     break;
-                case 82:
-                    this.cameraPos[1]+=1.0;
-                    this.mPresence.setPosition(this.cameraPos);
+                case "82": // R -- raise camera
+                case "33": // page up
+                    //this.cameraPos[1]+=1.0;
+                    this.mPresence.setVelocity([0, 30, 0]);
+                    //this.mPresence.setPosition(this.cameraPos);
                     break;
-                case 76:
-                    this.cameraPos[1]-=1.0;
-                    this.mPresence.setPosition(this.cameraPos);
+                case "70": // F -- lower camera
+                case "34": // page down
+                    //this.cameraPos[1]-=1.0;
+                    this.mPresence.setVelocity([0, -30, 0]);
+                    //this.mPresence.setPosition(this.cameraPos);
                     break;
+                case "81":
+                case "37": // left arrow: look left
+                    this.cameraPointX -= 10;
+                    var q = this._euler2Quat(this.cameraPointX * -.25, this.cameraPointY * -.25, 0);
+                    this.mPresence.setOrientation(q);
+                    break;
+                case "69":
+                case "39": // right arrow: look right
+                    this.cameraPointX += 10;
+                    var q = this._euler2Quat(this.cameraPointX * -.25, this.cameraPointY * -.25, 0);
+                    this.mPresence.setOrientation(q);
+                    break;
+                case "38S": // shift+up: look up
+                    this.cameraPointY -= 10;
+                    var q = this._euler2Quat(this.cameraPointX * -.25, this.cameraPointY * -.25, 0);
+                    this.mPresence.setOrientation(q);
+                    break;
+                case "40S": // shift+down: look down
+                    this.cameraPointY += 10;
+                    var q = this._euler2Quat(this.cameraPointX * -.25, this.cameraPointY * -.25, 0);
+                    this.mPresence.setOrientation(q);
+                    break;
+                case "67": // C -- reset camera & object
+                case "32": // space
+                    this.cameraPos = this.cameraPosInit.concat();
+                    this.cameraOrient = this.cameraOrientInit.concat();
+                    this.cameraPointX = this.cameraPointXInit;
+                    this.cameraPointY = this.cameraPointYInit;
+                    this.objectOrient = this.objectOrientInit.concat();
+                    this.objectScale = this.objectScaleInit.concat();
+                    this.updateCamera();
+                    this.updateObject();
+                    break;
+            }
+        }
+        if (msg.msg == "wheel") {
+            this.cameraPos = this.mPresence.position(new Date());
+            if (this.leftDown || this.middleDown || this.rightDown) 
+                return;
+            var ang = this.cameraPointX * -.25 * 0.0174532925;
+            var x = Math.sin(ang) * 5;
+            var y = Math.cos(ang) * 5;
+            if (msg.event.dy > 0) {
+                this.cameraPos[0] -= x;
+                this.cameraPos[2] -= y;
+                this.mPresence.setPosition(this.cameraPos);
+            }
+            else {
+                this.cameraPos[0] += x;
+                this.cameraPos[2] += y;
+                this.mPresence.setPosition(this.cameraPos);
             }
         }
     };
