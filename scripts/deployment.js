cea5dc40fa9e69577b6d50ce95bb6495c93db23c
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2010-10-23 11:05:58 -0700

    Add a deployment config script to customize a particular deployment. Currently it just separates out the chat server URL from the rest of the code.

---
+++ scripts/deployment.js
@@ -0,0 +1,7 @@
+/** This file contains deployment-specific configuration for the chat
+ *  page. Ideally, everything that needs to be modified to make this
+ *  run on different servers is isolated to this file.
+ */
+
+SpaceURL = "loop://localhost";
+//SpaceURL = "sirikata://localhost";
