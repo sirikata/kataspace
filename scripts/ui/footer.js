73fe6430fe252899f3cb5e6abf2c9ec22742e743
Patrick Horn <patrick.horn@gmail.com>
2011-02-17 18:57:40 -0800

    "position:fixed" fixes scrollbar appearing because of github button.

--- scripts/ui/footer.js
+++ scripts/ui/footer.js
@@ -31,26 +31,6 @@
  */
 
 FooterUI = function(elem) {
-    var footerHeight = 0,
-    footerTop = 0,
     $footer = elem;
-
-    positionFooter();
-
-    function positionFooter() {
-        footerHeight = $footer.height();
-        footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
-
-        if ( ($(document.body).height()+footerHeight) < $(window).height()) {
-            $footer
-                .css({ position: "absolute", top: footerTop });
-        } else {
-            $footer.css({ position: "static" });
-        }
-
-    }
-
-    $(window)
-        .scroll(positionFooter)
-        .resize(positionFooter);
+    $footer.css({ position: "fixed", bottom: "10px" });
 };
